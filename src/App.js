dba944998ca9ccc4ebb8a758583385e25d525433
charuka herath <charuka09@gmail.com>
2019-03-21 17:31:20 +0530

    states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import Table from './Table';
-import './index.css';
 
-class App extends Component{
-    render(){
-        return(
-            <div className="container">
-                <Table />
+class App extends Component {
+    render() {
+        state = {
+            characters: [
+                {
+                    name: 'Charlie',
+                    job: 'Janitor',
+                },
+                {
+                    name: 'Mac',
+                    job: 'Bouncer',
+                },
+                {
+                    name: 'Dee',
+                    job: 'Aspring actress',
+                },
+                {
+                    name: 'Dennis',
+                    job: 'Bartender',
+                },
+            ]
+        }
+
+        removeCharacters = index => {
+            const {characters} = this.state;
+            this.setState({
+                characters: characters.filter((character , i) => {
+                    return i !== index;
+                })
+            })
+        }
+
+        return (
+            <div className="container-fluid">
+                <Table characterDate={characters} removeCharacters={this.removeCharacters}/>
             </div>
         )
     }
